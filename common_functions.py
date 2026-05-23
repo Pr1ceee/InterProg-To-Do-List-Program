ddf6f3ef0825cf9818c81b22d244f34e4d388789
Ally Lumanlan <alyssazeanne.lumanlan@fcpc.edu.ph>
2026-05-24 07:09:26 +0800

    Module for common functions.

---
+++ common_functions.py
@@ -0,0 +1,45 @@
+# List of months
+months = [
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+]
+
+# List of days per month
+days_per_month = [
+    31, 28, 31,
+    30, 31, 30,
+    31, 31, 30,
+    31, 30, 31
+]
+
+# Function to check if task name is all numbers
+def is_all_numbers(text):
+    if len(text) == 0:
+        return False
+    
+    numbers = "0123456789"
+    for char in text:
+        if char not in numbers:
+            return False
+    return True
+
+# Function to check duplicate tasks
+def is_duplicate(task, tasks_list):
+    for existing_task in tasks_list:
+        if task.lower() == existing_task.lower():
+            return True      
+    return False
+
+# Function to display tasks with a custom title
+def display_tasks(title="VIEW TASK"):
+    print("=" * 40)
+    print(title.center(40))
+    print("=" * 40)
+    
+    if len(tasks_list) == 0:
+        print("No tasks found.")
+    else:
+        for i in range(len(tasks_list)):
+            print("[" + str(i + 1) + "]", tasks_list[i], "- Deadline:", deadline_list[i])
